e5f0e7298bff9c042b54ea5b871c94359e5f3387
Arthi K Rajan <43614625+Arthi01@users.noreply.github.com>
2020-06-29 08:37:26 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,18 +1,17 @@
 # Coursera Data Science: Exploratory Data Analysis
 # Course project 1: plot3
-# Cheng-Han Yu
 ################################################################################
-## load the data
+## Load the data
 rm(list = ls())
 data <- read.table("household_power_consumption.txt", header = T, 
                    sep = ";", na.strings = "?")
-# convert the date variable to Date class
+# Convert the date variable to Date class
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
 # Subset the data
 data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-# Convert dates and times
+# Convert the dates and times
 data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
 
 # Plot 3
